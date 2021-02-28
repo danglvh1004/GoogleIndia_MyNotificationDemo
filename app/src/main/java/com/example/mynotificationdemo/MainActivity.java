78aa28211d3a1966df6dcab9fd6eaa2c7897a527
danglvh1004 <lvhd.hdlv@gmail.com>
2021-02-28 21:20:01 +0700

    Commit

--- app/src/main/java/com/example/mynotificationdemo/MainActivity.java
+++ app/src/main/java/com/example/mynotificationdemo/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.mynotificationdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void doSomething(View view) {
+        NotificationManagerCompat myManager = NotificationManagerCompat.from(this);
+
+        NotificationCompat.Builder myNoti = new NotificationCompat.Builder(this);
+        myNoti.setContentTitle("1 New Message");
+        myNoti.setContentText("Hi How are you?");
+        myNoti.setSmallIcon(android.R.drawable.ic_btn_speak_now);
+
+        Intent i1 = new Intent(MainActivity.this, MainActivity.class);
+        PendingIntent pd = PendingIntent.getActivity(this, 1, i1, 0);
+        myNoti.setContentIntent(pd);
+        myNoti.setAutoCancel(true);
+
+        myManager.notify(1, myNoti.build());
+        finish();
     }
 }
